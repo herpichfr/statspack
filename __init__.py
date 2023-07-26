548c4a2800148914d0365cbf0df97b3a6ae9dd80
herpichfr <fabiorafaelh@gmail.com>
2023-07-26 16:49:30 +0100

    Initialize necessary arguments for the module

--- __init__.py
+++ __init__.py
@@ -1,2 +1,17 @@
-# package with modules for statistic visualization
-# herpich 2022-12-20 fabiorafaelh@gmail.com
+# statspack/__init__.py
+
+# Import sub-modules or functions
+from .bining import bining
+from .confidence_interval import find_confidence_interval
+from .density_contour import density_contour
+from .contour_pdf import contour_pdf
+
+# Define what names are imported with 'from statspack import *'
+__all__ = ['bining', 'find_confidence_interval',
+           'density_contour', 'contour_pdf']
+
+# Optional initializations or configurations
+# For example, you can set up logging here if needed
+import logging
+
+logging.basicConfig(level=logging.INFO)
