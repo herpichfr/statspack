ec679a09778e227768bf6195bc8141c2259c1dfa
herpichfr <fabiorafaelh@gmail.com>
2023-07-26 17:13:21 +0100

    Correct __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # statspack/__init__.py
 
 # Import sub-modules or functions
-from .bining import bining
-from .confidence_interval import find_confidence_interval
+from .binning import binning
+from .find_confidence_interval import find_confidence_interval
 from .density_contour import density_contour
 from .contour_pdf import contour_pdf
 
 # Define what names are imported with 'from statspack import *'
-__all__ = ['bining', 'find_confidence_interval',
+__all__ = ['binning', 'find_confidence_interval',
            'density_contour', 'contour_pdf']
 
 # Optional initializations or configurations
